1a51d0e199b138bb297db93a4d4d664755bf0e79
mattkelsey <mattkelseymk@gmail.com>
2016-04-12 19:31:31 -0700

    Add basic camera and movement mechanics

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+    return gulp.src('*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['lint', 'scripts', 'watch']);
