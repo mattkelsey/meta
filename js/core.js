4a5b9cb9e3f28d776bbfb2b17f72e3f31fa238f7
mattkelsey <mattkelseymk@gmail.com>
2016-04-13 19:47:29 -0700

    Fix movement mechanics

--- js/core.js
+++ js/core.js
@@ -1,18 +1,6 @@
 var scene, camera, renderer;
 
-var moveForward,
-    moveBackward,
-    moveLeft,
-    moveRight,
-    canJump;
-
-var hasPointerLock = checkForPointerLock();
-var velocity = new THREE.Vector3();
 var clock = new THREE.Clock();
-function checkForPointerLock() {
-    return 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
-}
-
 
 var controller;
 
@@ -28,7 +16,6 @@ function generateSurface(u, v) {
 }
 
 function init() {
-    initControls();
 
     scene = new THREE.Scene();
     var width = window.innerWidth;
@@ -43,7 +30,7 @@ function init() {
     camera.position.set(1,1,50);
 
     controller = new controll3r(camera, scene);
-    controller.init(document);
+    controller.init();
 
     window.addEventListener('resize', function() {
         var width = window.innerWidth,
@@ -78,57 +65,6 @@ function init() {
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    controller.update(clock);
+    controller.updatePos(clock);
 }
 
-function initControls() {
-    document.addEventListener('keydown', onKeyDown, false);
-    document.addEventListener('keyup', onKeyUp, false);
-}
-
-
-function onKeyDown(e) {
-    switch (e.keyCode) {
-    case 38: // up
-    case 87: // w
-        moveForward = true;
-        break;
-    case 37: // left
-    case 65: // a
-        moveLeft = true;
-        break;
-    case 40: // down
-    case 83: // s
-        moveBackward = true;
-        break;
-    case 39: // right
-    case 68: // d
-        moveRight = true;
-        break;
-    case 32: // space
-        if (canJump === true) velocity.y += 350;
-        canJump = false;
-        break;
-    }
-}
-
-function onKeyUp(e) {
-    switch(e.keyCode) {
-    case 38: // up
-    case 87: // w
-        moveForward = false;
-        break;
-    case 37: // left
-    case 65: // a
-        moveLeft = false;
-        break;
-    case 40: // down
-    case 83: // s
-        moveBackward = false;
-        break;
-    case 39: // right
-    case 68: // d
-        moveRight = false;
-        break;
-    }
-}
--- js/mv.js
+++ js/mv.js
@@ -1,21 +1,24 @@
 function controll3r(camera, scene) {
     this.controls = new THREE.PointerLockControls(camera);
-    this.controlsEnabled;
-    this.init = function (document) {
+    this.controlsEnabled = true;
+    this.hasPointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+
+    this.init = function () {
         scene.add(this.controls.getObject());
-        console.log(this.controls)
+        //console.log(this.controls)
         var element = document.body;
-        if (hasPointerLock) {
-            console.log(this.controls.enabled);
+        this.controls.enabled = true;
+        if (this.hasPointerLock) {
             var pointerlockchange = function (event) {
                 if (document.pointerLockElement === element ||
                     document.mozPointerLockElement === element ||
                     document.webkitPointerLockElement === element) {
                     this.controlsEnabled = true;
-                    this.controls.enabled = true;
+                    //controls.enabled = true;
                 } else {
                     this.controlsEnabled = false;
-                    this.controls.enabled = false;
+                    //controls.enabled = false;
                 }
             };
 
@@ -39,30 +42,38 @@ function controll3r(camera, scene) {
         } else {
             element.innerHTML = 'Bad browser; No pointer lock';
         }
+
+        document.addEventListener('keydown', onKeyDown, false);
+        document.addEventListener('keyup', onKeyUp, false);
+
+
     };
 
-    this.moveForward;
-    this.moveBackward;
-    this.moveLeft;
-    this.moveRight;
-    this.canJump;
-    //var hasPointerLock = checkForPointerLock();
-    this.velocity = new THREE.Vector3()
+    this.walkingSpeed = 2000.0;
+    this.jumpHeight = 450;
+    var moveForward,
+        moveBackward,
+        moveLeft,
+        moveRight,
+        canJump,
+        jumping;
 
-    this.update = function(clock) {
+    //var hasPointerLock = checkForPointerLock();
+    var velocity = new THREE.Vector3();
+
+    this.updatePos = function(clock) {
         if (this.controlsEnabled) {
             var delta = clock.getDelta();
-            var walkingSpeed = 2000.0;
 
             velocity.x -= velocity.x * 10.0 * delta;
             velocity.z -= velocity.z * 10.0 * delta;
             velocity.y -= 9.8 * 100.0 * delta;
 
-            if (moveForward) velocity.z -= walkingSpeed * delta;
-            if (moveBackward) velocity.z += walkingSpeed * delta;
-            if (moveLeft) velocity.x -= walkingSpeed * delta;
-            if (moveRight) velocity.x += walkingSpeed * delta;
-
+            if (moveForward) velocity.z -= this.walkingSpeed * delta;
+            if (moveBackward) velocity.z += this.walkingSpeed * delta;
+            if (moveLeft) velocity.x -= this.walkingSpeed * delta;
+            if (moveRight) velocity.x += this.walkingSpeed * delta;
+            if (jumping) velocity.y += this.jumpHeight; jumping = false;
             this.controls.getObject().translateX(velocity.x * delta);
             this.controls.getObject().translateY(velocity.y * delta);
             this.controls.getObject().translateZ(velocity.z * delta);
@@ -73,5 +84,54 @@ function controll3r(camera, scene) {
                 canJump = true;
             }
         }
-    }
+    };
+
+    function onKeyDown (e) {
+        console.log("keydown");
+        switch (e.keyCode) {
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = true;
+            break;
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+        case 32: // space
+            if (canJump === true) jumping = true;
+            canJump = false;
+            break;
+        }
+    };
+
+    function onKeyUp (e) {
+        switch(e.keyCode) {
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+        }
+    };
+
+
 }
